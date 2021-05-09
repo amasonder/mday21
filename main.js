4ae2a9988c01d2034f5802245808de555b25210d
amasonder <amasonder@gmail.com>
2021-05-09 19:53:31 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', function () {
+    const para4 = document.querySelector('#para4');
+    gif1.innerHTML = "<img src='img/babypush.gif'>";
+});
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', function () {
+    const para4 = document.querySelector('#para4');
+    gif2.innerHTML = "<img src='img/ace.gif'>";
+});
+
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', function () {
+    const para4 = document.querySelector('#para4');
+    gif3.innerHTML = "<img src='img/expel.gif'>";
+});
+
+const btn4 = document.querySelector('#btn4');
+btn4.addEventListener('click', function () {
+    const para4 = document.querySelector('#para4');
+    gif4.innerHTML = "<img src='img/spin.gif'>";
+});
